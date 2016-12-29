a3d0b107d76a8c2f2acf716c6a49bb3f98f99eb0
Emmanuel Asaber <emmanuelasaber@gmail.com>
2016-12-29 12:51:38 +0000

    Initial Commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Resolutions</title>
+	<link rel="stylesheet" href="{{URL::asset('css/style.css')}}">
+</head>
+<body>
+
+	<section class="wrapper">
+		<div class="new_task">
+			<input type="text" placeholder="New Resolution">
+		</div>
+		<ul class="todo-controls">
+			
+		</ul>
+
+	</section>
+	
+
+	<script src="{{url::asset('js/jquery.js')}}"></script>
+	<script src="{{url::asset('js/script.js')}}"></script>
+</body>
+</html>
